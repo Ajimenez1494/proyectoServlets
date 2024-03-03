8825fd65539c139fb3322a35b8150742d7b92c69
[Ajimenez1494] <[alij545@gmail.com]>
2024-03-03 19:30:05 +0100

    primer commit

--- src/main/java/daos/UsuarioDao.java
+++ src/main/java/daos/UsuarioDao.java
@@ -5,10 +5,15 @@ import org.apache.logging.log4j.Logger;
 
 import jakarta.persistence.EntityManager;
 
-
 public class UsuarioDao {
 	private static final Logger logger = LogManager.getLogger(UsuarioDao.class);
 	private static EntityManager em;
 
+	public UsuarioDao() {
+		em = JpaUtil.getEM("hibernateMySQL");
+	}
 
+	public UsuarioDao(String persitenceUnitName) {
+		em = JpaUtil.getEM("hibernateMySQL");
+	}
 }
